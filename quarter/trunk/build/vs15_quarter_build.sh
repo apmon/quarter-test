a3400919144e72401f958aa95b830f6426bb17e7
rohrlich <rohrlich@799e7b52-c93c-4235-a987-eab4da8661e3>
2016-08-23 19:35:53 +0000

    bug fixes

--- quarter/trunk/build/vs15_quarter_build.sh
+++ quarter/trunk/build/vs15_quarter_build.sh
@@ -2,7 +2,10 @@
 # Adjust the following lines for each build type (/x64, /debug, etc) & Qt version
 
 export BITS=64
-export BUILD_TYPE=$1
+if [ "$BUILD_TYPE" == "" ]; then
+echo "release me"
+  export BUILD_TYPE=release
+fi
 export MSVS_EXT=msvs2015-$BITS
 export BLD_EXT=qt56
 export QTDIR=C:/Qt/5.6.1/5.6/msvc2015_64
@@ -19,14 +22,13 @@ export QTDIR=`cygpath "$QTDIR"`
 
 export CPPFLAGS="-I$QTDIR/include/QtCore -I$QTDIR/include/QtWidgets -I$QTDIR/include/QtOpenGL -I$QTDIR/include/QtGui"
 
-export CONFIG_QTLIBS="-lQt5Core -lQt5Widgets -lQt5OpenGL -lQt5Gui -lQt5Designer -lQt5UiTools -lQt5Xml -lqtmain -lgdi32"
+export CONFIG_QTLIBS="-lQt5Core -lQt5Widgets -lQt5OpenGL -lQt5Gui -lQt5Xml -lqtmain -lgdi32"
 
-export QTR_BLD_DIR=/cygdrive/c/Quarter/build/cyg-$BLD_EXT-$MSVS_EXT-$BUILD_TYPE
+export QTR_BLD_DIR=/cygdrive/c/src/quarter/build/$BUILD_TYPE
 mkdir $QTR_BLD_DIR
 cd $QTR_BLD_DIR
 
-# Disabling pkg-config seems to be fairly important:
-if [ "$BUILD_TYPE" == "release" ] || [ "$BUILD_TYPE" == "" ]; then
+if [ "$BUILD_TYPE" == "release" ]; then
 echo "this is a release build!"
 /cygdrive/c/src/quarter/configure --disable-pkgconfig --with-qt=$QTDIR --prefix=$COINDIR --with-coin=$COINDIR --with-msvcrt=md --disable-debug
 fi
